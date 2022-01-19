d42f8b42c1111c4d2feef6d09a9799fd146a4569
DavidFGDH <32119775+DavidFGDH@users.noreply.github.com>
2022-01-19 12:15:44 -0600

    Add files via upload

---
+++ practica4/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">
+      TODO DGCx
+    </h1>
+    <todo-app/>
+  </div>
+</template>
+
+<script>
+import TodoApp from './components/TodoApp'
+export default {
+  name: 'App',
+  components: {
+    TodoApp
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ practica4/src/components/TodoApp.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1 class="my-5 text-center">Proyecto de Lista de Tareas</h1>
+  <todo-form/>
+  <todo-list/>
+</template>
+
+<script>
+import {provide,ref,watchEffect} from "vue";
+import TodoForm from './TodoForm'
+import TodoList from './Todolist'
+export default {
+  components:{
+    TodoForm,
+    TodoList
+  },
+  setup(){
+    const tareas =ref([])
+    provide('tareas',tareas)
+
+    if(localStorage.getItem('tareas')){
+      tareas.value = JSON.parse(localStorage.getItem('tareas'))
+    }
+    watchEffect(()=>{
+      localStorage.setItem('tareas',JSON.stringify(tareas.value))
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
